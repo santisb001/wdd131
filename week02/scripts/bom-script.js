07b4825ab915a912fe393f48f10a4cf0d1b5f385
Santiago Sburlati <santiagomatiassburlati@gmail.com>
2026-09-11 15:00:24 -0300

    w02 activity 3

--- week02/scripts/bom-script.js
+++ week02/scripts/bom-script.js
@@ -10,15 +10,25 @@ deleteButton.textContent = '❌';
 
 li.append(deleteButton);
 list.append(li);
-
+//You "listen" (wait) for the user to make a click
 button.addEventListener("click",function(){
+//check if the user actually entered something
 if (input.value.trim() !== ''){
     const li = document.createElement('li');
-}
-});
-
-deleteButton.addEventListener("click", function(){
+    li.textContent = input.value;
+    //Create a delete button for the li element
+    deleteButton.addEventListener("click", function(){
     list.removeChild(li);
     input.focus();
 });
+//Add the button to the list
+li.appendChild(deleteButton);
+list.appendChild(li);
+//Clear the user input field
+input.value = "";
+}
+input.focus();
+});
+
+
 
