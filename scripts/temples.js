827b53f5b409be07d117abe2aaf607fcd4ed4a5f
Santiago Sburlati <santiagomatiassburlati@gmail.com>
2026-09-16 16:40:17 -0300

    added the ham menu

--- scripts/temples.js
+++ scripts/temples.js
@@ -8,3 +8,12 @@ year.innerHTML = `©${today.getFullYear()}</span>`;
 
 //last modified thing
 document.getElementById("lastModified").textContent = document.lastModified;
+
+//Hamburguer menu
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector("nav ul");
+
+hamButton.addEventListener("click", () => {
+	navigation.classList.toggle("open");
+	hamButton.classList.toggle("open");
+});
